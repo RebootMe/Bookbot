005d59ee8701ca1a71a345e67c51733c46e97429
Quad98 <zeeshan98@gmail.com>
2024-10-05 18:37:10 +0500

    Alphabet count added

--- main.py
+++ main.py
@@ -3,12 +3,37 @@ def main():
     with open("Books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    k=total_words(file_contents)
-    print(k)
+    words=total_words(file_contents)
+    print(words)
+
+    alphabets=total_alphabets(file_contents.lower())
+    return print(alphabets)
 
 
 def total_words(text):
     words=text.split()
     return len(words)
+
+def total_alphabets(text):
+    count={
+        'a':0,'b':0,'c':0,
+        'd':0,'e':0,'f':0,
+        'g':0,'h':0,'i':0,
+        'j':0,'k':0,'l':0,
+        'm':0,'n':0,'o':0,
+        'p':0,'q':0,'r':0,
+        's':0,'t':0,'u':0,
+        'v':0,'w':0,'x':0,
+        'y':0,'z':0
+    }
+
+    for a in text:
+        for x in count:
+            if a==x:
+                #print(f"{a} is {x}")
+                count[x]=count[x]+1
+
+    return count
+
 
 main()
